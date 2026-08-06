c661e001e92868eaf31c2ea9aaea2ee6c29acf51
Michael Adler <therisen06@gmail.com>
2026-08-06 11:27:50 +0200

    ci: use nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,39 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
+  outputs =
+    { nixpkgs, ... }:
+    let
 
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nodejs tree-sitter ];
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+
+    in
+    {
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+
+          nativeBuildInputs = [
+            pkgs.git
+            pkgs.just
+            pkgs.cargo
+            pkgs.nodejs
+          ];
+
+          buildInputs = [
+            pkgs.tree-sitter
+          ];
+
           shellHook = ''
             export PATH=$PATH:$(pwd)/node_modules/.bin
           '';
         };
-
       });
+
+    };
 }
